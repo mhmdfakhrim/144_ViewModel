d21f98ddcc1f75a39a0ae54578b4c7a879507269
mhmdfakhrim <muhfakhri4@gmail.com>
2023-10-25 07:52:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kotlinio/CobaViewModel.kt
@@ -0,0 +1,36 @@
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.kotlinio.Data.DataForm
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class CobaViewModel: ViewModel() {
+    var namaUsr : String by mutableStateOf("")
+        private set
+    var noTlp : String by mutableStateOf("")
+        private set
+    var jenisKl : String by mutableStateOf("")
+        private set
+    var alamat : String by mutableStateOf("")
+        private set
+    private val _uiState = MutableStateFlow(DataForm())
+    val uiState: StateFlow<DataForm> = _uiState.asStateFlow()
+
+    fun BacaData(nm: String, tlp: String, jk: String, address:String){
+        namaUsr=nm;
+        noTlp=tlp;
+        jenisKl=jk;
+        alamat=address;
+    }
+    fun setJK(selectJK:String){
+        _uiState.update { currentState -> currentState.copy(sex= selectJK)}
+    }
+
+    fun BacaData(nm: String, tlp: String, jk: String) {
+
+    }
+}
---
+++ app/src/main/java/com/example/kotlinio/Data/DataSource.kt
@@ -0,0 +1,9 @@
+package com.example.kotlinio.Data
+import com.example.kotlinio.R
+
+object DataSource {
+    val jenis = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
